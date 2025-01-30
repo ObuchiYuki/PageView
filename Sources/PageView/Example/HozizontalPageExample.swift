21eb7908fa6f6da3f8e2ec9281346126dec2397f
ObuchiYuki <yukibochi1@gmail.com>
2025-01-30 17:57:08 +0900

    first commit

---
+++ Sources/PageView/Example/HozizontalPageExample.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  PageView
+//
+//  Created by yuki on 2025/01/30.
+//
+
+import SwiftUI
+
+fileprivate struct Item: Identifiable {
+    let id: Int
+    let color: Color
+}
+
+fileprivate struct HorizontalPageExample: View {
+    let items: [Item] = [
+        Item(id: 0, color: .red),
+        Item(id: 1, color: .green),
+        Item(id: 2, color: .blue),
+    ]
+    @State var selection: Item.ID = 0
+
+    var body: some View {
+        HorizontalPage(
+            items: items,
+            spacing: 16,
+            selection: $selection
+        ) { item in
+            item.color
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+    }
+}
+
+#Preview {
+    HorizontalPageExample()
+}
---
+++ Sources/PageView/HorizontalPage.swift
@@ -0,0 +1,128 @@
+//
+//  HorizontalPage.swift
+//  PageView
+//
+//  Created by yuki on 2025/01/30.
+//
+
+
+import SwiftUI
+
+public struct HorizontalPage<Item: Identifiable, Page: View>: UIViewControllerRepresentable {
+    public let items: [Item]
+    
+    public let page: (Item) -> Page
+    
+    public let spacing: CGFloat
+    
+    @Binding public var selection: Item.ID
+
+    public init(
+        items: [Item],
+        spacing: CGFloat,
+        selection: Binding<Item.ID>,
+        page: @escaping (Item) -> Page
+    ) {
+        self.items = items
+        self.spacing = spacing
+        self._selection = selection
+        self.page = page
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+
+    public func makeUIViewController(context: Context) -> UIPageViewController {
+        let options: [UIPageViewController.OptionsKey: Any] = [.interPageSpacing: self.spacing]
+        let controller = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal,
+            options: options
+        )
+        controller.dataSource = context.coordinator
+        controller.delegate = context.coordinator
+        if
+            let item = self.items.first(where: { $0.id == self.selection }),
+            let initialVC = context.coordinator.makeViewController(for: item)
+        {
+            controller.setViewControllers([initialVC], direction: .forward, animated: false)
+        }
+        return controller
+    }
+
+    public func updateUIViewController(_ uiViewController: UIPageViewController, context: Context) {
+        if
+            let currentVC = uiViewController.viewControllers?.first as? HostingControllerWrapper<Item, Page>,
+            currentVC.item.id != self.selection,
+            let item = self.items.first(where: { $0.id == self.selection }),
+            let newVC = context.coordinator.makeViewController(for: item)
+        {
+            let currentIndex = self.items.firstIndex(where: { $0.id == currentVC.item.id }) ?? 0
+            let newIndex = self.items.firstIndex(where: { $0.id == self.selection }) ?? 0
+            let direction: UIPageViewController.NavigationDirection = (newIndex >= currentIndex) ? .forward : .reverse
+            uiViewController.setViewControllers([newVC], direction: direction, animated: true)
+        }
+    }
+
+    public class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+        public let parent: HorizontalPage
+
+        public init(parent: HorizontalPage) {
+            self.parent = parent
+        }
+
+        fileprivate func makeViewController(for item: Item) -> HostingControllerWrapper<Item, Page>? {
+            return HostingControllerWrapper(rootView: self.parent.page(item), item: item)
+        }
+
+        public func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController
+        ) -> UIViewController? {
+            guard let vc = viewController as? HostingControllerWrapper<Item, Page>,
+                  let currentIndex = self.parent.items.firstIndex(where: { $0.id == vc.item.id }),
+                  currentIndex > 0
+            else { return nil }
+            let prevItem = self.parent.items[currentIndex - 1]
+            return self.makeViewController(for: prevItem)
+        }
+
+        public func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController
+        ) -> UIViewController? {
+            guard let vc = viewController as? HostingControllerWrapper<Item, Page>,
+                  let currentIndex = self.parent.items.firstIndex(where: { $0.id == vc.item.id }),
+                  currentIndex < (self.parent.items.count - 1)
+            else { return nil }
+            let nextItem = self.parent.items[currentIndex + 1]
+            return self.makeViewController(for: nextItem)
+        }
+
+        public func pageViewController(
+            _ pageViewController: UIPageViewController,
+            didFinishAnimating finished: Bool,
+            previousViewControllers: [UIViewController],
+            transitionCompleted completed: Bool
+        ) {
+            guard completed,
+                  let currentVC = pageViewController.viewControllers?.first as? HostingControllerWrapper<Item, Page>
+            else { return }
+            self.parent.selection = currentVC.item.id
+        }
+    }
+}
+
+private class HostingControllerWrapper<Item, Content: View>: UIHostingController<Content> {
+    let item: Item
+
+    init(rootView: Content, item: Item) {
+        self.item = item
+        super.init(rootView: rootView)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
